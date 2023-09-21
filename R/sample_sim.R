82e4d7451e6a3556841c34606050c40db738ac67
Lane McCoy <lanemccoy2024@gmail.com>
2023-09-20 21:46:17 -0500

    Sample_sim function

---
+++ R/sample_sim.R
@@ -0,0 +1,28 @@
+#' @title Sample Simulations
+#'
+#' @param x Number of items sampled
+#' @param n Sample size
+#' @param iter Number of iterations
+#' @param time Time between each iteration
+#'
+#' @return A plot of proportions of x selected in each individual iteration
+#' @export
+#'
+#' @examples
+#' sample_sim(x=10, n=100, iter=10, time=0.5)
+sample_sim=function(x, n, iter=10,time=0.5){
+  for( i in 1:iter){
+    #Make a sample
+    s=sample(1:x,n,replace=TRUE)
+    #Convert the sample into a factor
+    sf=factor(s,levels=1:x)
+    #Make a barplot
+    barplot(table(sf)/n,beside=TRUE,col=rainbow(10),
+            main=paste("Example sample()", " iteration ", i, " n= ", n,sep="") ,
+            ylim=c(0,0.5)
+    )
+
+    #Timer between tables
+    Sys.sleep(time)
+  }
+}
