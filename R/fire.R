709e4a43e674b4bd6b1191ecbaccfaf2ef81bc18
Lane McCoy <lanemccoy2024@gmail.com>
2023-10-16 16:10:21 -0500

    fire

---
+++ R/fire.R
@@ -0,0 +1,11 @@
+#' Fire Damage Dataset
+#'
+#' A dataset collected regarding home damage caused by fires.
+#'
+#' @format A dataframe with 15 rows and two variables
+#' \describe{
+#'    \item{DISTANCE}{Distance of the fire from the station in miles}
+#'    \item{DAMAGE}{X 1000 Dollars in damage caused by the fire}
+#' }
+#' @source \url{https://www.crcpress.com/Statistics-for-Engineering-and-the-Sciences/Mendenhall-Sincich/p/book/9781498728850}
+"fire"
